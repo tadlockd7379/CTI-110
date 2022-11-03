cc33e9dc5efccb0199da722022834deffcfbbceb
tadlockd7379 <117186380+tadlockd7379@users.noreply.github.com>
2022-11-03 10:50:46 -0400

    change code

--- p1/P1HW2.py
+++ p1/P1HW2.py
@@ -3,32 +3,27 @@
 # Drew Tadlock
 # 11/3
 
-def main():
-    budget = number_input('What\'s your total budget? ')
-    destination = input('Where do you want to travel? ') 
-    gas = number_input('How much are you spending on gas? ')
-    accomodations = number_input('How much are you spending on accomodations? ')
-    food = number_input('How much are you spending on food? ')
-
-    expenses = gas + accomodations + food
-    result = budget - expenses
-
-    print(f'\nDestination: {destination}')
-    print(f'Total Budget: ${budget}\n')
-    print(f'Gas: ${gas}')
-    print(f'Accomdations: ${accomodations}')
-    print(f'Food: ${food}\n')
-    print(f'Remaining Balance: ${result}\n')
-
-    main()
-
-def number_input(ask):
+def input_int(ask):
     result = input(ask)
     try:
         result = int(result)
     except:
         print('Invalid number.')
-        number_input(ask)
+        input_int(ask)
     return result
 
-main()
+budget = input_int('What\'s your total budget? ')
+destination = input('Where do you want to travel? ') 
+gas = input_int('How much are you spending on gas? ')
+accomodations = input_int('How much are you spending on accomodations? ')
+food = input_int('How much are you spending on food? ')
+
+expenses = gas + accomodations + food
+result = budget - expenses
+
+print(f'\nDestination: {destination}')
+print(f'Total Budget: ${budget}\n')
+print(f'Gas: ${gas}')
+print(f'Accomdations: ${accomodations}')
+print(f'Food: ${food}\n')
+print(f'Remaining Balance: ${result}\n')
